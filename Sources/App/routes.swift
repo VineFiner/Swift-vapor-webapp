57971490451fac1b3219a51b182f40b578c74f3d
vine <1953892532@qq.com>
2018-08-25 18:36:00 +0800

    Add Leaf

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,4 +12,16 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    // "It works" page
+    router.get { req in
+        return try req.view().render("welcome")
+    }
+    
+    // Says hello
+    router.get("hello", String.parameter) { req -> Future<View> in
+        return try req.view().render("hello", [
+            "name": req.parameters.next(String.self)
+            ])
+    }
 }
